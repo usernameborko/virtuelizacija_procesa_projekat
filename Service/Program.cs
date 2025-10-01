604c3efd1c33ad92514f2b3a772fda95fca13963
usernameborko <radovic.borko21@gmail.com>
2025-10-01 23:00:32 +0200

    Konfiguracija WCF servisa

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Service
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(IChargingService));
+            host.Open();
+
+            Console.WriteLine("Charging service is running...");
+            Console.WriteLine("Press any key to stop the service.");
+            Console.ReadKey();
+
+            host.Close();
+            Console.WriteLine("Service stopped.");
         }
     }
 }
