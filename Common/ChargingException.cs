bd23b06a517f4e74889bb7124e551c113293796f
usernameborko <radovic.borko21@gmail.com>
2025-10-01 22:27:50 +0200

    Kreiranje ServiceContract interfejsa i custom exceptiona

---
+++ Common/ChargingException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ChargingException
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        public ChargingException(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
---
+++ Common/IChargingService.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IChargingService
+    {
+        [OperationContract]
+        bool StartSession(string vehicleId);
+
+        [OperationContract]
+        bool PushSample(ChargingData data);
+
+        [OperationContract]
+        bool EndSession(string vehicleId);
+    }
+}
