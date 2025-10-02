eed494ac5fe5411ff507efec1bc309f8d44734ae
usernameborko <radovic.borko21@gmail.com>
2025-10-02 09:08:10 +0200

    EventLogger klasa i subscribe-ovanje na dogadjaje

---
+++ Service/EventLogger.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+
+namespace Service
+{
+    public class EventLogger
+    {
+        public void OnTransferStarted(object sender, ChargingEventArgs e)
+        {
+            Console.WriteLine($"[EVENT] TRANSFER STARTED: {e.Message} at {e.Timestamp}");
+        }
+
+        public void OnSampleReceived(object sender, ChargingEventArgs e)
+        {
+            Console.WriteLine($"[EVENT] SAMPLE RECEIVED: {e.Message} at {e.Timestamp}");
+        }
+
+        public void OnTransferCompleted(object sender, ChargingEventArgs e)
+        {
+            Console.WriteLine($"[EVENT] TRANSFER COMPLETED: {e.Message} at {e.Timestamp}");
+        }
+        public void OnWarningRaised(object sender, ChargingEventArgs e)
+        {
+            Console.WriteLine($"[WARNING] {e.Message} at {e.Timestamp}");
+        }
+    }
+}
--- Service/Program.cs
+++ Service/Program.cs
@@ -7,10 +7,18 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            EventLogger logger = new EventLogger();
+
+            ChargingService.OnTransferStarted += logger.OnTransferStarted;
+            ChargingService.OnSampleReceived += logger.OnSampleReceived;
+            ChargingService.OnTransferCompleted += logger.OnTransferCompleted;
+            ChargingService.OnWarningRaised += logger.OnWarningRaised;
+
             ServiceHost host = new ServiceHost(typeof(ChargingService));
             host.Open();
 
             Console.WriteLine("Charging service is running...");
+            Console.WriteLine("Event logging is active.");
             Console.WriteLine("Press any key to stop the service.");
             Console.ReadKey();
 
